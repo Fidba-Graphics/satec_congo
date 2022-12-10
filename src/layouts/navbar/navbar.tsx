627c81ba1078a2f6b524ccbcf77c7deffce3177d
BenMukebo <l.benkasmukebo7@gmail.com>
2022-12-10 17:26:42 +0200

    Build navbar layout component

--- src/layouts/navbar/navbar.tsx
+++ src/layouts/navbar/navbar.tsx
@@ -1,98 +1,37 @@
-import React, { useState } from "react";
-// import logo from "../../assets/images/logo.png";
-// import './navbar.scss';
+import React from "react";
 
-// type ChildProp = {
-//   onClickProp?: React.MouseEventHandler
-// }
+import { FaFacebookF } from 'react-icons/fa'; //BsFacebook
+import { AiOutlineTwitter, AiOutlineDribbble } from 'react-icons/ai';
+import { TbBrandInstagram } from 'react-icons/tb';
+import { BiPaperPlane } from 'react-icons/bi';
 
-type NavMenu = {
-  name: string;
-  linkTo: string;
-  // onClickItem?: React.MouseEventHandler<Element> | undefined;
-  // onClickItem?:  () => void;
-  // onClickItem?: React.MouseEventHandler<HTMLAnchorElement>;
-  // onClickItem?: (event: React.MouseEvent<HTMLAnchorElement>) => void;
-}
-
-// type NavProps = {
-//   NavMenu: {
-//     name: string;
-//     linkTo: string;
-//   }[],
-//   // onClickItem?: React.MouseEventHandler<Element> | undefined;
-//   // onClickItem?:  () => void;
-//   // onClickItem?: React.MouseEventHandler<HTMLAnchorElement>;
-//   onClickItem?: (event: React.MouseEvent<HTMLAnchorElement>) => void;
-// }
-
-//   const [isOpen, setIsOpen] = useState(false);
-//   const toggle = () => setIsOpen(!isOpen);
-//   const close = () => setIsOpen(false);
-
-
-// const Links: React.FC<{ links: NavMenu[] }> = ({ links, onClickItem }) => {
-// return (
-  
-// )
-// };
+import styles from './navbar.module.scss';
 
 export const Navbar: React.FC<{}> = ( ) => {
-  const MenuLinks: any[] = [
-    {
-      name: 'A propos',
-      linkTo: 'about-section'
-    },
-    {
-      name: 'Produits',
-      linkTo: 'products-section'
-    },
-    {
-      name: 'Team',
-      linkTo: 'team-section'
-    },
-    {
-      name: 'Contacts',
-      linkTo: 'contacts'
-    }
-  ]
-  const [BurgerClick, setBurgerClick] = useState<boolean>(false);
-  const [navMenu, setNavMenu] = useState<string>("navbar-container container");
-
-  const handleBurgerClick = () => {
-    if(!BurgerClick) {
-      setNavMenu('navbar-container container open')
-    }
-    else {
-      setNavMenu('navbar-container container')
-    }
-    setBurgerClick(!BurgerClick)
-  }
 
   return (
-    <nav className="navbar">
-      <div className={navMenu}>
-          <div className="hamburger-lines" onClick={handleBurgerClick}>
-              <span className="line line1"></span>
-              <span className="line line2"></span>
-              <span className="line line3"></span>
-          </div>
-          <ul className="menu-items">
-          {MenuLinks.map((menu: NavMenu) => (
-            <li key={menu.name} className="menu">
-              <a href={`#${menu.linkTo}`} onClick={handleBurgerClick}>
-                  {menu.name}
-              </a>
-            </li>
-          ))}
-        </ul>
-        <div className="logo">
-          MAGNUSBEN
-          <a href="#home-section">
-             {/* <img src={logo} alt="logo" /> */}
-           </a>
-        </div>
+    <nav className={styles.navbar}>
+      <div className={styles.contact_info}>
+       <p className={styles.contact_number}>+243 994 764 613</p>
+       <p className={styles.contact_email}> 
+       <span><BiPaperPlane/></span>
+       info@sateccongo.com</p>
       </div>
+
+      <ul className={styles.social_media_wrapper}>
+        <li className={styles.social_media}>
+          <a href="#"><FaFacebookF/></a>
+        </li>
+        <li className={styles.social_media}>
+          <a href="#"><AiOutlineTwitter/></a>
+        </li>
+        <li className={styles.social_media}>
+          <a href="#"><TbBrandInstagram/></a>
+        </li>
+        <li className={styles.social_media}>
+          <a href="#"><AiOutlineDribbble/></a>
+        </li>
+      </ul>
     </nav> 
   )
 }
